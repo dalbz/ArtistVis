7b0fd6129674b89835bc7d4616d13179fc938456
Dave <david.albert09@gmail.com>
2013-01-14 19:13:39 -0500

    Modify to retrieve 100 related artists for each of my top 50 artists from the past year - save to text file

--- src/ArtistVis.java
+++ src/ArtistVis.java
@@ -1,9 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashSet;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -15,20 +21,45 @@ public class ArtistVis {
      */
     public static void main(String[] args) {
 
-        ArrayList<String> similar = getSimilarArtists("Animal Collective");
-        for (String artist : similar) {
-            System.out.println(artist);
-            try {
-                Thread.sleep(210);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        generateArtistList();
+
+    }
+
+    private static void generateArtistList(){
+
+        File artistList = new File("artists.txt");
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(artistList));
+
+            ArrayList<String> topArtists = getTopArtists("dalbz");
+            HashSet<String> allArtists = new HashSet<String>();
+
+            allArtists.addAll(topArtists);
+
+            int i = 1;
+            for (String artist : topArtists) {
+                allArtists.addAll(getSimilarArtists(artist));
+                System.out.println(i);
+                i++;
             }
-            System.out.println(getTags(artist));
+
+            Gson gson = new Gson();
+
+            writer.write(gson.toJson(allArtists));
+
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
     }
 
     private static String getHttpRequest(String urlText) {
+
+        pause();
 
         String output = "";
 
@@ -60,7 +91,7 @@ public class ArtistVis {
 
         String response = getHttpRequest("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="
                 + artistName
-                + "&api_key=56b26f4bbdd972b4460ca2ab0b53e63a&format=json");
+                + "&limit=100&api_key=56b26f4bbdd972b4460ca2ab0b53e63a&format=json");
 
         JsonParser parser = new JsonParser();
 
@@ -75,6 +106,30 @@ public class ArtistVis {
         }
 
         return similarArtists;
+
+    }
+
+    private static ArrayList<String> getTopArtists(String userName) {
+
+        ArrayList<String> topArtists = new ArrayList<String>();
+
+        String response = getHttpRequest("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user="
+                + userName
+                + "&limit=50&period=12month&api_key=56b26f4bbdd972b4460ca2ab0b53e63a&format=json");
+
+        JsonParser parser = new JsonParser();
+
+        JsonObject root = parser.parse(response).getAsJsonObject();
+
+        JsonArray artists = root.getAsJsonObject("topartists")
+                .getAsJsonArray("artist");
+
+        for (int i = 0; i < artists.size(); i++) {
+            JsonObject artist = artists.get(i).getAsJsonObject();
+            topArtists.add(artist.get("name").getAsString());
+        }
+
+        return topArtists;
 
     }
 
@@ -105,6 +160,15 @@ public class ArtistVis {
 
         return tagList;
 
+    }
+
+    private static void pause() {
+        try {
+            Thread.sleep(210);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
